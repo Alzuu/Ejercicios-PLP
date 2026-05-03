7030338b40ff6fd8d6ea91e6898d03b9bd97537e
Alzuu <santi.alzuguren@gmail.com>
2026-05-03 08:16:40 -0300

    TPN5 Haskell ejercicios 6 - 9

--- TPN5/misFunciones.hs
+++ TPN5/misFunciones.hs
@@ -17,4 +17,42 @@ cuantosIgualesDeDos :: Int -> Int -> Int
 cuantosIgualesDeDos a b
                         | a == b = 2
                         | otherwise = 0
-                    
+
+-- Ejercicio 5
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial(n - 1)
+
+-- Sucesión Extrana:
+sumatoria :: Integer -> Integer -> Integer
+sumatoria 0 _ = 0
+sumatoria i n = i^n + sumatoria (i - 1) n
+
+sucesionDosNumeros :: Integer -> Integer -> Double
+sucesionDosNumeros x n = realToFrac(sumatoria n n) / realToFrac(factorial x)
+
+--Ejercicio 6
+nAnd :: Bool -> Bool -> Bool
+nAnd True True = False
+nAnd  _    _   = True
+
+--Ejercicio 7
+funny :: Integer -> Integer -> Integer -> Bool
+funny x y z 
+    | x > z = True
+    | x >= y = False
+    | otherwise = True
+
+funnySimpl :: Integer -> Integer -> Integer -> Bool
+funnySimpl x y z = x > z || x < y
+
+--Ejercicio 8
+allDiferent :: Int -> Int -> Int -> Bool
+allDiferent x y z = x /= y && x /= z && y /= z
+
+--Ejercicio9
