d9a322b3b413b638733e9331baad2d92f5c224c9
Alzuu <santi.alzuguren@gmail.com>
2026-05-03 09:30:45 -0300

    TPN5 Haskell ejercicios 9 - 15

--- TPN5/misFunciones.hs
+++ TPN5/misFunciones.hs
@@ -17,8 +17,9 @@ cuantosIgualesDeDos :: Int -> Int -> Int
 cuantosIgualesDeDos a b
                         | a == b = 2
                         | otherwise = 0
-
+-----------------------------------------------------------------------------------------
 -- Ejercicio 5
+
 fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
@@ -28,7 +29,7 @@ factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial(n - 1)
 
--- Sucesión Extrana:
+-- Sucesión Extraña:
 sumatoria :: Integer -> Integer -> Integer
 sumatoria 0 _ = 0
 sumatoria i n = i^n + sumatoria (i - 1) n
@@ -36,12 +37,16 @@ sumatoria i n = i^n + sumatoria (i - 1) n
 sucesionDosNumeros :: Integer -> Integer -> Double
 sucesionDosNumeros x n = realToFrac(sumatoria n n) / realToFrac(factorial x)
 
---Ejercicio 6
+-----------------------------------------------------------------------------------------
+-- Ejercicio 6
+
 nAnd :: Bool -> Bool -> Bool
 nAnd True True = False
 nAnd  _    _   = True
 
---Ejercicio 7
+-----------------------------------------------------------------------------------------
+-- Ejercicio 7
+
 funny :: Integer -> Integer -> Integer -> Bool
 funny x y z 
     | x > z = True
@@ -51,8 +56,65 @@ funny x y z
 funnySimpl :: Integer -> Integer -> Integer -> Bool
 funnySimpl x y z = x > z || x < y
 
---Ejercicio 8
+-----------------------------------------------------------------------------------------
+-- Ejercicio 8
+
 allDiferent :: Int -> Int -> Int -> Bool
 allDiferent x y z = x /= y && x /= z && y /= z
 
---Ejercicio9
+-----------------------------------------------------------------------------------------
+-- Ejercicio 9
+
+allDiferentWrong :: Int -> Int -> Int -> Bool
+allDiferentWrong x y z = x /= y && y /= z
+
+-----------------------------------------------------------------------------------------
+-- Ejercicio 10
+
+cuartaPotencia :: Int -> Int
+cuartaPotencia n = n * n * n * n
+
+cuartaPotencia1 :: Int -> Int 
+cuartaPotencia1 n = n^4
+
+cuartaPotencia2 :: Int -> Int
+cuartaPotencia2 n = alCuadrado n * alCuadrado n
+
+alCuadrado :: Int -> Int 
+alCuadrado n = n^2
+
+-----------------------------------------------------------------------------------------
+-- Ejercicio 12
+
+charAInt :: Char -> Int
+charAInt c = fromEnum c - fromEnum '0' 
+
+-----------------------------------------------------------------------------------------
+-- Ejercicio 13
+
+digitoRomano :: Char -> String
+digitoRomano '1' = "I"
+digitoRomano '2' = "II"
+digitoRomano '3' = "III"
+digitoRomano '4' = "IV"
+digitoRomano '5' = "V"
+digitoRomano '6' = "VI"
+digitoRomano '7' = "VII"
+digitoRomano '8' = "VIII"
+digitoRomano '9' = "IX"
+digitoRomano _ = ""
+
+-----------------------------------------------------------------------------------------
+-- Ejercicio 14 
+
+entreLineas :: String -> String -> String -> String
+entreLineas a b c = a ++ "\n" ++ b  ++ "\n" ++ c ++ "\n"
+
+-- Acordarse de poner putStr en la terminal.
+
+-----------------------------------------------------------------------------------------
+-- Ejercicio 15
+
+duplicar :: String -> Integer -> String
+duplicar _ 0 = "\n"
+duplicar s n = s ++ " " ++ duplicar s (n-1)
